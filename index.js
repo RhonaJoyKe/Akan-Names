f6b5f0f7b5ec88c71734e9c0488ddc9f9ccc4e9a
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-09-04 14:44:58 +0300

    adds Js Functionality

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
-var date=document.getElementById("DOB").Value;
-console.log(date);
+const maleNames = [{
+    Sunday: "Kwasi",
+
+    Monday: "Kwadwo",
+
+    Tuesday: "Kwabena",
+
+    Wednesday: "Kwaku",
+
+    Thursday: "Yaw",
+
+    Friday: "Kofi",
+
+    Saturday: "Kwame"
+}
+]
+const femaleNames = [{
+    Sunday: "Akosua",
+
+    Monday: "Adwoa",
+
+    Tuesday: "Abenaa",
+
+    Wednesday: "Akua",
+
+    Thursday: "Yaa",
+
+    Friday: " Afua",
+
+    Saturday: "Ama"
+}
+]
+
+
+document.getElementById("buu").addEventListener(("click"), function () {
+    var date = document.querySelector("#DOB").value;
+    var gender = document.getElementsByName("gender");
+    var dateFinal = new Date(date)
+    if (document.getElementById("male").checked) {
+        alert("I am  a male");
+        var weekDay = dateFinal.toLocaleDateString("EN-US", { weekday: 'long' })
+        if (weekDay ==) {
+
+        }
+        if (document.getElementById("female").checked) {
+            alert("I am  a female");
+        }
+
+    })
